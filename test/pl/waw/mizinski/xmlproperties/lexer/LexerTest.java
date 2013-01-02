2dd8f2b879b467f3f35a30b613aba1302ebab42c
Konrad Miziński <mizin@mizin-K52Jr.(none)>
2013-01-02 21:00:25 +0100

    przymiarki do testów

---
+++ test/pl/waw/mizinski/xmlproperties/lexer/LexerTest.java
@@ -0,0 +1,14 @@
+package pl.waw.mizinski.xmlproperties.lexer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class LexerTest
+{
+	@Test
+	public void validateXML(){
+		assertNull(null);
+	}
+}
